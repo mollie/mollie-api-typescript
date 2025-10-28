17fab75d5eca5980cc47040acb7f7dc17497a130
André Oliveira <andre.oliveira@mollie.com>
2025-10-28 15:29:28 +0000

    Add Custom User Agent Header Support

--- src/hooks/mollie-hooks.ts
+++ src/hooks/mollie-hooks.ts
@@ -1,17 +1,17 @@
 import { RequestInput } from "../lib/http.js";
 import { BeforeCreateRequestHook, HookContext } from "./types.js";
+import { SDK_METADATA } from "../lib/config.js";
 
 export class MollieHooks implements BeforeCreateRequestHook {
-    beforeCreateRequest(_: HookContext, input: RequestInput): RequestInput {
-        const idempotencyKey = "Idempotency-Key";
-
+    beforeCreateRequest(hookContext: HookContext, input: RequestInput): RequestInput {
         // Always normalize to Headers
-        const headers = new Headers(input.options?.headers);
+        let headers = new Headers(input.options?.headers);
 
-        // Only add if missing or empty
-        if (!headers.has(idempotencyKey) || !headers.get(idempotencyKey)) {
-            headers.set(idempotencyKey, this.generateIdempotencyKey());
-        }
+        // Handle Idempotency-Key
+        headers = this.handleIdempotencyKey(headers);
+
+        // Customize Customize the User Agent header
+        headers = this.customizeUserAgent(headers, hookContext);
 
         return {
             ...input,
@@ -20,6 +20,38 @@ export class MollieHooks implements BeforeCreateRequestHook {
                 headers,
             },
         };
+    }
+
+    private handleIdempotencyKey(headers: Headers): Headers {
+        const idempotencyKey = "Idempotency-Key";
+
+        // Only add if missing or empty
+        if (!headers.has(idempotencyKey) || !headers.get(idempotencyKey)) {
+            headers.set(idempotencyKey, this.generateIdempotencyKey());
+        }
+
+        return headers;
+    }
+
+    private customizeUserAgent(headers: Headers, hookContext: HookContext): Headers {
+        const userAgentKey = "User-Agent";
+
+        const customUserAgent = hookContext.options.customUserAgent;
+
+        const genVersion = SDK_METADATA.genVersion;
+        const sdkVersion = SDK_METADATA.sdkVersion;
+        const nodeVersion = typeof process !== "undefined" && process.version ? process.version : "unknown";
+        const packageName = SDK_METADATA.userAgent.split(" ").at(-1);
+
+        let mollieUserAgent = `Speakeasy/${genVersion} TypeScript/${nodeVersion} ${packageName}/${sdkVersion}`;
+        if (customUserAgent) {
+            mollieUserAgent = `${mollieUserAgent} ${customUserAgent}`;
+        }
+
+        console.log("Setting User-Agent to:", mollieUserAgent);
+
+        headers.set(userAgentKey, mollieUserAgent);
+        return headers;
     }
 
     private generateIdempotencyKey(): string {
